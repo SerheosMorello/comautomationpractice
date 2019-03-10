a41f7943e3c5e2cd118bd924a957714ed0bf7331
Sergii.Pankrashyn <Sergii.Pankrashyn.9@fntsoftware.com>
2019-03-10 18:48:24 +0200

    add count search result

--- src/test/java/homework/TestSuite.java
+++ src/test/java/homework/TestSuite.java
@@ -3,12 +3,10 @@ package homework;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-public class TestSuite {
 @RunWith(Suite.class)
 @Suite.SuiteClasses({AccountTest.class, SearchTest.class })
-public class JunitTestSuite {
+public class TestSuite {
     }
 
-}
 
 
